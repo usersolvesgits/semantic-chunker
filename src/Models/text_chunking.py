f66e4b47e44303a2a574a6f2089876e1ef2cb3cf
user <francesco.bianchini08@gmail.com>
2026-04-01 13:53:09 +0200

    Code commit

--- src/Models/text_chunking.py
+++ src/Models/text_chunking.py
@@ -1,3 +1,21 @@
+from typing import List
+from transformers import AutoTokenizer as AT
+
 class TextChunking:
-    def __init__(self) -> None:
-        ...
+    @staticmethod
+    def chunck_text(testo, max_tokens=100, overlap=20) -> List[str]:
+        tokenizer = AT.from_pretrained("bert-base-uncased")
+        tokens = tokenizer.encode(testo, add_special_tokens=False)
+
+        chunks = []
+        startIndex = 0
+
+        while startIndex <  len(tokens):
+            endIndex = startIndex + max_tokens
+            chunk = tokens[startIndex::endIndex]
+
+            chunks.append(tokenizer.decode(chunk))
+
+            startIndex += max_tokens - overlap
+
+        return chunks
